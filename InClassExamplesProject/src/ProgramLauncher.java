5271f04d3371e0e79096f7893e4360dba42054be
melissa.e.wilson <melissa.e.wilson@PLB122-45751.occc.edu>
2025-03-31 18:56:34 -0500

    First commit.

---
+++ InClassExamplesProject/src/ProgramLauncher.java
@@ -0,0 +1,15 @@
+
+public class ProgramLauncher {
+	
+	public static int mySum(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello! This my first repo!");
+		
+		System.out.println(mySum(1,2));
+	}
+
+}
